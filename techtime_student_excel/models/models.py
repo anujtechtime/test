7371004c0c2a3fcfc406872e5f129e147d3c7883
Anuj Chauhan <anuj.chauhan@finbii.com>
2022-11-09 15:42:23 +0530

    aded

--- techtime_student_excel/models/models.py
+++ techtime_student_excel/models/models.py
@@ -285,7 +285,7 @@ class TechtimeStudentexcel(models.Model):
             registered = 0
             for material_line in self:
                 print("lllllllllllllllllllll",material_line.partner_id)
-                year_all = self.env["year.year"].search([],order='year desc')
+                year_all = self.env["year.year"].search([],order='year asc')
                 _logger.info("pincode************11111111111111#####**%s" %year_all)
                 for yrs in year_all:
                     print("yrs$$$$$$$$$$$$$$$$$$$$$$$",yrs.year)
