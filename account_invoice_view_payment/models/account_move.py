ef861cf3d3f47fea370a0b4e76f716fd6383a0e2
Anuj Chauhan <anuj.chauhan@finbii.com>
2022-11-17 14:25:25 +0530

    added

--- account_invoice_view_payment/models/account_move.py
+++ account_invoice_view_payment/models/account_move.py
@@ -2,7 +2,7 @@
 #        (https://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models
+from odoo import models, api, _
 import logging
 
 _logger = logging.getLogger(__name__)
