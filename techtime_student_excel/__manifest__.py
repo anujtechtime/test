aaf9e557ef41e9c9391cbdbbbc16ed8e42705b2a
Anuj Chauhan <anuj.chauhan@finbii.com>
2022-11-17 18:03:08 +0530

    added

--- techtime_student_excel/__manifest__.py
+++ techtime_student_excel/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'user_group.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- techtime_student_excel/models/models.py
+++ techtime_student_excel/models/models.py
@@ -688,6 +688,26 @@ class ResData(models.Model):
     Father_Job = fields.Char("Father Job")
     work_address_father = fields.Char("Work address")
 
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+                        toolbar=False, submenu=False):
+        result = super(ResData, self).fields_view_get(
+            view_id=view_id, view_type=view_type,
+            toolbar=toolbar, submenu=submenu)
+
+        # Disabling the import button for users who are not in import group
+        if view_type == 'tree':
+            doc = etree.XML(result['arch'])
+            if not self.env.user.has_group('techtime_student_excel.group_manager_import_button'):
+             # When the user is not part of the import group
+                for node in doc.xpath("//tree"):
+                    # Set the import to false
+                    node.set('import', 'false')
+            result['arch'] = etree.tostring(doc)
+
+        return result 
+
     
     def action_done_show_wizard_level(self):
         for ddtsh in self:
