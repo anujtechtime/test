ade8a476f67e1d40b52755df5bca306bd1c19370
Anuj Chauhan <anuj.chauhan@finbii.com>
2022-11-17 18:08:55 +0530

    added

--- techtime_student_excel/__manifest__.py
+++ techtime_student_excel/__manifest__.py
@@ -27,7 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'user_group.xml'
+        'security/user_group.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
