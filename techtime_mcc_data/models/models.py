d6b361c5f08f7a5896b335ec222169a1943f29eb
Anuj Chauhan <anuj.chauhan@finbii.com>
2022-12-24 10:35:24 +0530

    added

--- techtime_mcc_data/models/models.py
+++ techtime_mcc_data/models/models.py
@@ -30,3 +30,32 @@ class CrmTeamDateAccount(models.Model):
     _inherit = "purchase.order.line"
 
     qty_received = fields.Float("Received Qty", related="product_qty", inverse='_inverse_qty_received', compute_sudo=True, store=True, digits='Product Unit of Measure')
+
+class ContractmDateAccount(models.Model):
+
+    _inherit = "hr.contract"
+
+    employ_type  = fields.Selection([('option1','تعيين - متقاعد'),
+        ('option2','تعيين - غير متقاعد مشمول بالضمان'),
+        ('option3','اعارة'),
+        ('option4', 'محاضر خارجي'),
+        ('option5','اجر يومي')], string="Employee Type")
+    basic_salary = fields.Float('Basic Salary')
+    social_security = fields.Float("Social Security")
+    tax = fields.Float("Tax")
+    tax_other = fields.Float("Tax Other")
+    traning = fields.Float("Traning")
+    allowance = fields.Float("Allowance")
+    day_deduction = fields.Float("Day-Deduction")
+
+class ContraPayslipDateAccount(models.Model):
+
+    _inherit="hr.payslip"
+
+    description = fields.Html("Description")
+
+
+
+
+
+
