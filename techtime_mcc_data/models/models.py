78d6997bc07231ed20ba574dc33ebde71a374d4d
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-01-02 14:48:50 +0530

    added

--- techtime_mcc_data/models/models.py
+++ techtime_mcc_data/models/models.py
@@ -41,18 +41,71 @@ class ContractmDateAccount(models.Model):
         ('option4', 'محاضر خارجي'),
         ('option5','اجر يومي')], string="Employee Type")
     basic_salary = fields.Float('Basic Salary')
+
+
     social_security = fields.Float("Social Security")
-    tax = fields.Float("Tax")
-    tax_other = fields.Float("Tax Other")
-    traning = fields.Float("Traning")
-    allowance = fields.Float("Allowance")
-    day_deduction = fields.Float("Day-Deduction")
+
+    compensation = fields.Float("Compensation")
+
+
+    married_with_house_wife = fields.Boolean("Married Male with house wife")
+    married_with_working_wife = fields.Boolean("Married Male with working wife")
+
+    married_with_non_working_husband = fields.Boolean("Married Female with non-working husband")
+    married_with_working_husband = fields.Boolean("Married Female with working husband")
+
+
+
+    single_male = fields.Boolean("Single Male")
+
+    male_female_with_children = fields.Integer("Female with No. of children")
+
+    divorced_male = fields.Boolean("Divorced Male")
+
+    sinle_female = fields.Boolean("Single Female")
+
+    divorced_female = fields.Boolean("Divorced Female")
+
+    if_age_is_above_63 = fields.Boolean("Age is above 63")
+
+    total_salary = fields.Float("Total Salary")
+
+
+
+    # tax = fields.Float("Tax")
+    # tax_other = fields.Float("Tax Other")
+    # traning = fields.Float("Traning")
+    # allowance = fields.Float("Allowance")
+    # day_deduction = fields.Float("Day-Deduction")
+
 
 
     @api.onchange('wage')
     def _inverse_wage(self):
         if self.wage:
-            self.basic_salary = float(self.wage) * 0.7
+            self.compensation = float(self.wage) * 0.3
+            self.social_security = float(self.wage) * 0.05
+            if self.married_with_house_wife:
+                self.total_salary = self.social_security + self.compensation + 375000
+            if self.married_with_working_wife:
+                self.total_salary = self.social_security + self.compensation + 208333
+            if self.married_with_non_working_husband:
+                self.total_salary = self.social_security + self.compensation + 416666
+            if self.married_with_working_husband:
+                self.total_salary = self.social_security + self.compensation + 208333
+            if self.single_male:
+                self.total_salary = self.social_security + self.compensation + 208333
+            if self.male_female_with_children:
+                self.total_salary = self.social_security + self.compensation + self.male_female_with_children * 16666
+            if self.divorced_male:
+                self.total_salary = self.social_security + self.compensation + 208333
+            if self.sinle_female:
+                self.total_salary = self.social_security + self.compensation + 255555
+            if self.divorced_female:
+                self.total_salary = self.social_security + self.compensation + 255555
+            if self.if_age_is_above_63:
+                self.total_salary = self.social_security + self.compensation + 25000
+
 
 
 
