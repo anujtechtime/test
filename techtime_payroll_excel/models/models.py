0ff33d0a938759364cdb1f45be5d0e1e23e31484
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-01-31 14:06:54 +0530

    added

--- techtime_payroll_excel/models/models.py
+++ techtime_payroll_excel/models/models.py
@@ -40,6 +40,27 @@ class techtime_payroll_excel(models.Model):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
 
+    def compute_sheet(self):
+        for payslip in self:
+            number = payslip.number or self.env["ir.sequence"].next_by_code(
+                "salary.slip"
+            )
+            # delete old payslip lines
+            payslip.line_ids.unlink()
+
+            # set the list of contract for which the rules have to be applied
+            # if we don't give the contract, then the rules to apply should be
+            # for all current contracts of the employee
+            contract_ids = payslip.contract_id.ids or self.get_contract(
+                payslip.employee_id, payslip.date_from, payslip.date_to
+            )
+            lines = [
+                (0, 0, line)
+                for line in self._get_payslip_lines(contract_ids, payslip.id)
+            ]
+            payslip.write({"line_ids": lines, "number": number})
+        # return payslip
+
 
     def send_mis_report(self):
         filename = 'Payslip.xls'
