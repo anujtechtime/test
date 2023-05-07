7f83638ecd5c3a25d5be974c0948679d1929029d
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-05-07 12:20:54 +0530

    added

--- techtime_payroll_excel/models/models.py
+++ techtime_payroll_excel/models/models.py
@@ -508,11 +508,112 @@ class techtime_payroll_excel(models.Model):
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
-#
+#hr.payslip
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+    def send_mis_report_techtime_data(self):
+        filename = 'Payslip.xls'
+        string = 'Payslip_report.xls'
+        wb = xlwt.Workbook(encoding='utf-8')
+        worksheet = wb.add_sheet(string)
+        header_bold = xlwt.easyxf("font: bold on; pattern: pattern solid, fore_colour gray25;")
+        cell_format = xlwt.easyxf()
+        filename = 'Payslip_Report_%s.xls' % date.today()
+        rested = self.env['hr.payslip'].search([])
+        row = 1
+        border_normal = xlwt.easyxf('borders: left thin, right thin, top thin, bottom thin; font: bold on; pattern: pattern solid, fore_colour gray25;')
+        border_1 = xlwt.easyxf('borders: left 1, right 1, top 1, bottom 1;')
+        border_2 = xlwt.easyxf('borders: left 2, right 2, top 2, bottom 2;')
+        border_color_2 = xlwt.easyxf('borders: top_color blue, bottom_color blue, right_color blue, left_color blue, left 2, right 2, top 2, bottom 2; font: bold on; pattern: pattern solid, fore_colour gray25;')
+        worksheet.col(0).width = 10000
+        worksheet.col(1).width = 15000
+        worksheet.col(2).width = 10000
+        worksheet.row(0).height = 500
+                   
+
+
+        worksheet.write(0, 0, 'Payslip Name', border_color_2)
+        worksheet.write(0, 1, 'Employee Name', border_color_2)
+        worksheet.write(0, 2, 'Date From', border_color_2)
+        worksheet.write(0, 3, 'Date To', header_bold)
+
+        worksheet.write(0, 4, 'الراتب الكلي', header_bold)
+        worksheet.write(0, 5, 'الراتب الاسمي1', header_bold)
+        worksheet.write(0, 6, 'التعويضية1', header_bold)
+        worksheet.write(0, 7, 'مجموع الاسمي والتعويضية', header_bold)
+        worksheet.write(0, 8, 'الضريبة', header_bold)
+        worksheet.write(0, 9, 'عدد الايام المستقطعة', header_bold)
+        worksheet.write(0, 10, 'مبلغ الايام المستقطعة', header_bold)
+        worksheet.write(0, 11, 'الضمان الاجتماعي', header_bold)
+        worksheet.write(0, 12, 'التدريب والتأهيل', header_bold)
+        worksheet.write(0, 13, 'مجموع الاستقطاعات', header_bold)
+        worksheet.write(0, 14, 'صافي الراتب1', header_bold)
+        # v.onboard_date >= (datetime.today().date().replace(day=1) - relativedelta(months=1)) and v.onboard_date <= (datetime.today().date() - relativedelta(months=1))
+        for material_line_id in self:
+            worksheet.write(row, 0, material_line_id.name or '')
+            worksheet.write(row, 1, material_line_id.employee_id.name or '')
+            worksheet.write(row, 2, material_line_id.date_from or '')
+            worksheet.write(row, 3, material_line_id.date_to or '')
+
+            for iit in material_line_id.line_ids:
+                if iit.code == "WAGE":
+                    worksheet.write(row, 4, iit.total or '')
+
+                if iit.code == "BSCC":
+                    worksheet.write(row, 5, iit.total or '')
+
+                if iit.code == "CMPS":
+                    worksheet.write(row, 6, iit.total or '')
+                if iit.code == "WAG":
+                    worksheet.write(row, 7, iit.total or '')
+                if iit.code == "TAX":    
+                    worksheet.write(row, 8, iit.total or '')
+                if iit.code == "day2":    
+                    worksheet.write(row, 9, iit.total or '')
+                if iit.code == "DDTA":
+                    worksheet.write(row, 10, iit.total or '')
+                if iit.code == "SST":    
+                    worksheet.write(row, 11, iit.total or '')
+                if iit.code == "TRA":    
+                    worksheet.write(row, 12, iit.total or '')
+                if iit.code == "TTD":    
+                    worksheet.write(row, 13, iit.total or '')
+                if iit.code == "NTS":    
+                    worksheet.write(row, 14, iit.total or '')    
+            row += 1
+        fp = io.BytesIO()
+        print("fp@@@@@@@@@@@@@@@@@@",fp)
+        wb.save(fp)
+        print(wb)
+        out = base64.encodebytes(fp.getvalue())
+        attachment = {
+                       'name': str(filename),
+                       'display_name': str(filename),
+                       'datas': out,
+                       'type': 'binary'
+                   }
+        ir_id = self.env['ir.attachment'].create(attachment) 
+        print("ir_id@@@@@@@@@@@@@@@@",ir_id)
+
+        xlDecoded = base64.b64decode(out)
+
+        # file_added = "/home/anuj/Desktop/workspace13/payslip_report.xlsx"
+        # with open(file_added, "wb") as binary_file:
+        #     binary_file.write(xlDecoded)
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        download_url = '/web/content/' + str(ir_id.id) + '?download=true'
+        return {
+            "type": "ir.actions.act_url",
+            "url": str(base_url) + str(download_url),
+            "target": "new",
+        }        
+
+        # df = pd.export_excel (r'/home/anuj/Desktop/workspace13/payslip_report.xlsx')
+        # print (df)              
+
 
     def compute_sheet(self):
         for payslip in self:
