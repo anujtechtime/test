697fd5777ebda42ccc6800c76f1b5e60da81da8a
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-05-19 01:15:16 +0530

    added

--- techtime_mcc_data/__manifest__.py
+++ techtime_mcc_data/__manifest__.py
@@ -24,11 +24,12 @@
 
     # always loaded
     'data': [
+    'security/security.xml',
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     'views/assets.xml',
-    'security/security.xml'
+
     ],
     # only loaded in demonstration mode
     'demo': [
