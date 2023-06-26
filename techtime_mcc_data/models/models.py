b339abc40f62fd3627e28973d2603d8ac6967b41
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-06-26 15:38:10 +0530

    added

--- techtime_mcc_data/models/models.py
+++ techtime_mcc_data/models/models.py
@@ -934,7 +934,7 @@ class ContraPayslipDateAccount(models.Model):
         
         header_bold_extra_tag = xlwt.easyxf("font: bold on; pattern: pattern solid, fore_colour green; font: color white; align: horiz centre")
 
-        header_bold_extra = xlwt.easyxf("font: bold on; pattern: pattern solid, fore_colour gray25; font: color white; align: horiz centre")
+        header_bold_extra = xlwt.easyxf("font: bold on; pattern: pattern solid, fore_colour gray25; font: color black; align: horiz centre")
 
         main_cell_total_of_total = xlwt.easyxf("font: bold off, color black;\
                      borders: top_color black, bottom_color black, right_color black, left_color black,\
