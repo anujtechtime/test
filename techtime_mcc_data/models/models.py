a866d56c6ce023af138bf85b83faa4ea1174f7b4
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-08-20 00:56:08 +0530

    added

--- techtime_mcc_data/models/models.py
+++ techtime_mcc_data/models/models.py
@@ -880,7 +880,7 @@ class DataLevelValueData(models.TransientModel):
             # print("levels_sale_order@@@@@@@@@@@@@@@@@@@@@@@@",levels_sale_order)
             _logger.info("eeeeeeeeeeeeeeeeeeee************11111111111111#####**%s" %self.id)
             levels_sale_order.remark_data_change  = (4, self.id)
-            self.res_partber = levels_sale_order.id
+            # self.res_partber = levels_sale_order.id
             if self.level:
                 levels_sale_order.level = self.level
                 # levels_sale_order.partner_id.level = self.level
