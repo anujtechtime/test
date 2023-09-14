948f0c3d44becaf7ca878cdc0a1d818db377cd04
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-09-14 10:18:31 +0530

    added

--- almaqal_templates/models/models.py
+++ almaqal_templates/models/models.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api , _ 
+from odoo.exceptions import UserError, ValidationError
 
 
-# class almaqal_templates(models.Model):
+class almaqal_templates(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            year_active  = self.env["year.year"].search([("active", "=", True)])
+            if self.year.id not in year_active.mapped("id"):
+                raise ValidationError(_('الطالب لم يرحل. هل تريد اكمال عملية التسجيل؟'))
 #     _name = 'almaqal_templates.almaqal_templates'
 #     _description = 'almaqal_templates.almaqal_templates'
 
