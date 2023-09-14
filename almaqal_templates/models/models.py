5aebd77d72a6fc2b0307101ff472d924239d4da8
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-09-14 12:18:04 +0530

    added the validtion

--- almaqal_templates/models/models.py
+++ almaqal_templates/models/models.py
@@ -13,6 +13,19 @@ class almaqal_templates(models.Model):
             year_active  = self.env["year.year"].search([("active", "=", True)])
             if self.year.id not in year_active.mapped("id"):
                 raise ValidationError(_('الطالب لم يرحل. هل تريد اكمال عملية التسجيل؟'))
+
+
+class almaqalPayment(models.Model):
+    _inherit = "account.payment"
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            year_active  = self.env["year.year"].search([("active", "=", True)])
+            if self.year.id not in year_active.mapped("id"):
+                raise ValidationError(_('الطالب لم يرحل. هل تريد اكمال عملية التسجيل؟'))                
+
+
 #     _name = 'almaqal_templates.almaqal_templates'
 #     _description = 'almaqal_templates.almaqal_templates'
 
