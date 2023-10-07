38ca20b2301f8f5f7cf4f03e7a49f58849966283
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-10-07 15:19:32 +0530

    added

--- almaqal_templates/models/models.py
+++ almaqal_templates/models/models.py
@@ -4,6 +4,10 @@ from odoo import models, fields, api , _
 from odoo.exceptions import UserError, ValidationError
 from googletrans import Translator
 import googletrans
+import base64
+from pdf2image import convert_from_path
+from rembg import remove 
+from PIL import Image 
 
 
 
@@ -54,7 +58,22 @@ class almaqalPayment(models.Model):
                     'title': "Warning", 
                     'message': 'الطالب لم يرحل. هل تريد اكمال عملية التسجيل؟', 
                     } 
-                }                
+                }       
+
+class ReportToImage(models.Model):
+    _inherit = "res.partner"
+
+    @api.onchange("year_born"):
+    def _onchange_year_born(self):
+        print_report = self.env.ref('almaqal_templates.report_payment_receipt_student_batch_card').sudo()._render_qweb_pdf(self.ids)
+        print("print_report#############",print_report)
+        # loan_lead.pdf_credit_score = base64.b64encode(print_report[0]) 
+
+
+        # images = convert_from_path('example.pdf')
+
+        # for i in range(len(images)):
+        #     images[i].save('page'+ str(i) +'.jpg', 'JPEG')                 
             #     raise UserError(_('الطالب لم يرحل. هل تريد اكمال عملية التسجيل؟'))
 #     _name = 'almaqal_templates.almaqal_templates'
 #     _description = 'almaqal_templates.almaqal_templates'
@@ -75,6 +94,78 @@ class Techtest(models.Model):
     name_english = fields.Char("English Name")
     batch_number = fields.Char("Batch Number")
     date_of_expiration = fields.Char("Date Of  Expiration")
+    image_stuent = fields.Binary("Image Student badge")
+
+    def _onchange_year_born(self):
+        print_report = self.env.ref('almaqal_templates.report_payment_receipt_student_batch_data').sudo().render_qweb_pdf(self.ids)
+        print("print_report#############",print_report)
+
+        pdf_credit_score = base64.b64encode(print_report[0]) 
+
+        print("pdf_credit_score@@@@@@@@@@@@@@@@@@@@@",pdf_credit_score)
+
+        f = open('sample.pdf', 'wb')
+
+
+        f.write(print_report[0])
+
+        images = convert_from_path('sample.pdf')
+
+
+        for i in range(len(images)):
+            images[i].save('page'+ str(i) +'.jpg') 
+ 
+  
+        # Store path of the output image in the variable output_path 
+        output_path = 'samples.png' 
+          
+        # Processing the image 
+        inputs = Image.open('page0.jpg') 
+          
+        # # Removing the background from the given Image 
+        # output = remove(inputs) 
+          
+        # #Saving the image in the given path 
+        # output.save(output_path)  
+
+        # img = Image.open("./image.png")
+        img = inputs.convert("RGBA")
+     
+        datas = img.getdata()
+     
+        newData = []
+     
+        for item in datas:
+            if item[0] == 255 and item[1] == 255 and item[2] == 255:
+                newData.append((255, 255, 255, 0))
+            else:
+                newData.append(item)
+     
+        img.putdata(newData)
+
+        print("img@@@@@@@@@@@@@@@@@@@@@@@@@@@",img)
+        img.save("./New.png", "PNG")
+        print("Successful")
+
+        print("img@@@@@@@@@@@@@@ccccccccccc",img)
+
+
+        def open_target_file(target_path):
+            with open(target_path,"rb") as excel_file:
+                return excel_file.read()
+
+        def encode_file(excel_file):
+            return base64.b64encode(excel_file)
+
+        def decode_file():
+            return base64.b64decode(excel_file)
+
+        destiny_path = ""
+
+        excel_file = open_target_file('New.png')
+        encoded_excel = encode_file(excel_file)
+
+        self.image_stuent = encoded_excel
 
     @api.onchange('name')
     def _onchange_name(self):
