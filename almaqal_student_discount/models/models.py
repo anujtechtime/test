491815491e9af26f1a4c2e8f3c87310816691a8c
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-10-18 13:42:09 +0530

    aded

--- almaqal_student_discount/models/models.py
+++ almaqal_student_discount/models/models.py
@@ -30,23 +30,39 @@ class InstallmentDetails(models.Model):
 class ResPrtner(models.Model):
     _inherit = 'sale.order'
 
+    # @api.onchange('partner_id')
+    # def _compute_partner_id(self):
+    #     print("college################eeeeeeeeeeeeeeee",self.college.student_discoubt) 
+    #     for cgpa in self.college.student_discoubt:
+    #         if self.partner_id.shift >=  cgpa.shift and self.partner_id.student_cgpa >=  cgpa.percentage_from and self.partner_id.student_cgpa <=  cgpa.percentage_to:
+    #             self.installment_amount = cgpa.amount
+    #             instamm_ment_details = self.env["installment.details"].search([("student_dicount","=",True),("installment","=",cgpa.amount)])
+    #             if instamm_ment_details:
+    #                 for i in instamm_ment_details.sale_installment_line_ids:
+    #                     sale_installment = self.sale_installment_line_ids.create({
+    #                         'number' : i.number,
+    #                         'payment_date' : i.payment_date,
+    #                         'amount_installment' : i.amount_installment,
+    #                         'description': 'Installment Payment',
+    #                         'sale_installment_id' : result.id,
+    #                         # "invoice_id" : invoice_id.id
+    #                         })
+
     @api.onchange('partner_id')
     def _compute_partner_id(self):
-        print("college################eeeeeeeeeeeeeeee",self.college.student_discoubt) 
-        for cgpa in self.college.student_discoubt:
-            if self.partner_id.shift >=  cgpa.shift and self.partner_id.student_cgpa >=  cgpa.percentage_from and self.partner_id.student_cgpa <=  cgpa.percentage_to:
-                self.installment_amount = cgpa.amount
-                instamm_ment_details = self.env["installment.details"].search([("student_dicount","=",True),("installment","=",cgpa.amount)])
-                if instamm_ment_details:
-                    for i in instamm_ment_details.sale_installment_line_ids:
-                        sale_installment = self.sale_installment_line_ids.create({
-                            'number' : i.number,
-                            'payment_date' : i.payment_date,
-                            'amount_installment' : i.amount_installment,
-                            'description': 'Installment Payment',
-                            'sale_installment_id' : result.id,
-                            # "invoice_id" : invoice_id.id
-                            })
+        # if self.partner_id.shift >=  cgpa.shift and self.partner_id.student_cgpa >=  cgpa.percentage_from and self.partner_id.student_cgpa <=  cgpa.percentage_to:
+        instamm_ment_details = self.env["installment.details"].search([("student_dicount","=",True),('college','=',self.college.id),('shift','=',self.shift),('year','=',self.year.id),('department','=',self.department.id),('percentage_from','>=',self.partne_id.final_result),('percentage_to','<=',self.partne_id.final_result)])
+        self.installment = instamm_ment_details.installment
+        if instamm_ment_details:
+            for i in instamm_ment_details.sale_installment_line_ids:
+                sale_installment = self.sale_installment_line_ids.create({
+                    'number' : i.number,
+                    'payment_date' : i.payment_date,
+                    'amount_installment' : i.amount_installment,
+                    'description': 'Installment Payment',
+                    'sale_installment_id' : result.id,
+                    # "invoice_id" : invoice_id.id
+                    })
 
 #     name = fields.Char()
 #     value = fields.Integer()
