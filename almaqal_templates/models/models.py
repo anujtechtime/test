a072eea27af801fa7e690aa369d984de740da94a
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-10-28 17:38:48 +0530

    added

--- almaqal_templates/models/models.py
+++ almaqal_templates/models/models.py
@@ -19,58 +19,58 @@ class almaqal_templates(models.Model):
     _inherit = "sale.order"
 
 
-    @api.model
-    def create(self, vals):
-        result = super(almaqal_templates, self).create(vals)
-        print("result##############",result)    
-        print("result.college###########",result.college,result.department,result.student, result.year)
-        installmet_dat = result.env["installment.details"].search([('college' , '=', result.college.id),("level","=",result.level),("Subject","=",result.Subject),('department','=',result.department.id),('Student','=',result.student.id),('year','=', result.year.id)],limit=1)
-        print("result.id#$$$$$$$$$$$$$$$",installmet_dat)
-        print("installmet_dat@@@@@@@@@@@@@@@",installmet_dat)
-        if installmet_dat:
-            print("sale_installment_line_ids########",installmet_dat.sale_installment_line_ids.ids)
-            # for datts in installmet_dat.sale_installment_line_ids:
-            # result.sale_installment_line_ids = [(6, 0, installmet_dat.sale_installment_line_ids.ids)]
-            result.installment_amount = installmet_dat.installment
-            # result.payable_amount = installmet_dat.installment / installmet_dat.installment_number
-            result.tenure_month = installmet_dat.installment_number
-            result.second_payment_date = datetime.today().date()
-            order_line = result.env['sale.order.line'].create({
-                'product_id': 1,
-                'price_unit': result.installment_amount,
-                'product_uom': result.env.ref('uom.product_uom_unit').id,
-                'product_uom_qty': 1,
-                'order_id': result._origin.id,
-                'name': 'sales order line',
-            })
-        failed_student = self.env["sale.order"].search([("partner_id","=",result.partner_id.id),("college","=",result.partner_id.college.id),("year","!=",result.partner_id.year.id),("level","=",result.partner_id.level)], limit=1)
-        print("failed_student@@@@@@@@@@@@@@@@",failed_student)
-        _logger.info("failed_student************11111111111111#####**%s" %failed_student)
-        if failed_student:
-            result.installment_amount = failed_student.installment_amount
-            for i in failed_student.sale_installment_line_ids:
-                installment = result.sale_installment_line_ids.create({
-                'number' : i.number,
-                'payment_date' : i.payment_date,
-                'amount_installment' : i.amount_installment,
-                'description': 'Installment Payment',
-                'sale_installment_id' : result.id,
-                # "invoice_id" : invoice_id.id
-                })  
-        if not failed_student and installmet_dat:
-            count = 0
-            for i in installmet_dat.sale_installment_line_ids:
-                sale_installment = result.sale_installment_line_ids.create({
-                    'number' : i.number,
-                    'payment_date' : i.payment_date,
-                    'amount_installment' : i.amount_installment,
-                    'description': 'Installment Payment',
-                    'sale_installment_id' : result.id,
-                    # "invoice_id" : invoice_id.id
-                    })
-                count = count + 1          
+    # @api.model
+    # def create(self, vals):
+    #     result = super(almaqal_templates, self).create(vals)
+    #     print("result##############",result)    
+    #     print("result.college###########",result.college,result.department,result.student, result.year)
+    #     installmet_dat = result.env["installment.details"].search([('college' , '=', result.college.id),("level","=",result.level),("Subject","=",result.Subject),('department','=',result.department.id),('Student','=',result.student.id),('year','=', result.year.id)],limit=1)
+    #     print("result.id#$$$$$$$$$$$$$$$",installmet_dat)
+    #     print("installmet_dat@@@@@@@@@@@@@@@",installmet_dat)
+    #     if installmet_dat:
+    #         print("sale_installment_line_ids########",installmet_dat.sale_installment_line_ids.ids)
+    #         # for datts in installmet_dat.sale_installment_line_ids:
+    #         # result.sale_installment_line_ids = [(6, 0, installmet_dat.sale_installment_line_ids.ids)]
+    #         result.installment_amount = installmet_dat.installment
+    #         # result.payable_amount = installmet_dat.installment / installmet_dat.installment_number
+    #         result.tenure_month = installmet_dat.installment_number
+    #         result.second_payment_date = datetime.today().date()
+    #         order_line = result.env['sale.order.line'].create({
+    #             'product_id': 1,
+    #             'price_unit': result.installment_amount,
+    #             'product_uom': result.env.ref('uom.product_uom_unit').id,
+    #             'product_uom_qty': 1,
+    #             'order_id': result._origin.id,
+    #             'name': 'sales order line',
+    #         })
+    #     failed_student = self.env["sale.order"].search([("partner_id","=",result.partner_id.id),("college","=",result.partner_id.college.id),("year","!=",result.partner_id.year.id),("level","=",result.partner_id.level)], limit=1)
+    #     print("failed_student@@@@@@@@@@@@@@@@",failed_student)
+    #     _logger.info("failed_student************11111111111111#####**%s" %failed_student)
+    #     if failed_student:
+    #         result.installment_amount = failed_student.installment_amount
+    #         for i in failed_student.sale_installment_line_ids:
+    #             installment = result.sale_installment_line_ids.create({
+    #             'number' : i.number,
+    #             'payment_date' : i.payment_date,
+    #             'amount_installment' : i.amount_installment,
+    #             'description': 'Installment Payment',
+    #             'sale_installment_id' : result.id,
+    #             # "invoice_id" : invoice_id.id
+    #             })  
+    #     if not failed_student and installmet_dat:
+    #         count = 0
+    #         for i in installmet_dat.sale_installment_line_ids:
+    #             sale_installment = result.sale_installment_line_ids.create({
+    #                 'number' : i.number,
+    #                 'payment_date' : i.payment_date,
+    #                 'amount_installment' : i.amount_installment,
+    #                 'description': 'Installment Payment',
+    #                 'sale_installment_id' : result.id,
+    #                 # "invoice_id" : invoice_id.id
+    #                 })
+    #             count = count + 1          
 
-        return result
+    #     return result
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
