8ccff8aa9925c6010fa3528d96b02d89060874b9
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-12-02 11:57:17 +0530

    added

--- comceptplus_pack_records/wizard/ir_model_download_pack.py
+++ comceptplus_pack_records/wizard/ir_model_download_pack.py
@@ -309,9 +309,150 @@ class ModelDownloadPack(models.TransientModel):
                 raise ValidationError(_('A ZIP file was not created:\n%s') % '\n'.join(msg))
 
 
+
+    def pack_act_getfile_batch_id(self):
+        Attachment = self.env['ir.attachment']
+
+        file_path = self.pack_create_file_path()
+        file_path = self.sanitize_path(file_path)
+        if file_path:
+            msg = []
+            counter = 0
+            for rec in self.pack_find_candidates():
+                # Process the structure
+                rec_folder = self.pack_render_folder_structure(rec, file_path)
+
+                print("This folder is %s" % rec_folder)
+
+                # Create a folder
+                try:
+                    self.create_folder(rec_folder)
+                except Exception as e:
+                    msg.append(_(e))
+                    _logger.error("Folder creation %s" % e)
+                    pass
+
+                # Copy attachments
+                if os.path.isdir(rec_folder):
+                    for att1 in self.env["res.partner"].search([("id",'=',rec.id)]):
+                        data_in  = self.env["res.partner"].search([("id",'=',rec.id)])
+                        if data_in.image_1920:
+                            encoded_string = data_in.image_1920
+                            decoded_string = b64decode(encoded_string)
+                            extension = imghdr.what(None, h=decoded_string)
+                            print("extension@@@@@@@@@@@@@@@@@@",extension)
+                            att = self.env["ir.attachment"].create({
+                                'res_model': self._context['active_model'],
+                                'res_id' : rec.id,
+                                'datas' : data_in.image_1920,
+                                'name': str(att1.batch) + "." + str(extension),
+                                })
+
+                            try:
+                                if att.db_datas:
+                                    f = open(rec_folder + '/' + att.name, 'wb')
+                                    f.write(Attachment._file_read(att.db_datas))
+                                    f.close()
+                                else:
+                                    print("AttachmentAttachmentAttachment@@@@@@@@@@@",Attachment._full_path(att.store_fname))
+                                    shutil.copy(
+                                                Attachment._full_path(att.store_fname),
+                                                os.path.join(rec_folder, att.name)
+                                                )
+                            except Exception as e2:
+                                msg.append(_(e2))
+                                _logger.error("File copy: %s" % e2)
+                                pass
+                            att.unlink()    
+
+                counter += 1
+                msg.append(_('%s files processed') % counter)
+
+            # Create the zip file
+            zip_created = False
+            print("counter@@@@@@@@@@@@@@",counter)
+            if counter:
+                try:
+                    zip_file = file_path.split(os.path.sep)[-1] + '.zip'
+                    print("11111111111111111111111")
+                    _logger.info("ZIP FILE NAME: %s" % zip_file)
+
+                    zip_file_path = os.path.join(self.pack_get_file_path(), zip_file)
+                    print("222222222222222222222")
+                    zipf = zipfile.ZipFile(zip_file_path, 'w')
+                    print("333333333333333333333333333")
+                    _logger.info("zip_file_path: %s" % zip_file_path)
+
+                    #aqui está o problema - qual a pasta onde deve de começar o zip?
+                    self.zipdir(file_path, zipf)
+                    print("44444444444444444444444444")
+                    zipf.close()
+                    print("5555555555555555555555555")
+                    zip_created = True
+                    print("6666666666666666666666666666")
+                    _logger.info("ZIP file (%s) was created in %s" % (zip_file, zip_file_path))
+                except Exception as e:
+                    _logger.error("ZIP creation: %s" % e)
+                    msg.append(_(e))
+                    pass
+
+            # Create an attachment for the zipped file
+            if zip_created:
+                try:
+                    af = open(zip_file_path, 'rb')
+                    datas = base64.encodestring(af.read())
+                    af.close()
+                    print("datas@@@@@@@@@@@@@@@@@222222222222",datas)
+                    att = Attachment.sudo().create({
+                                                    'name': zip_file,
+                                                    'type': 'binary',
+                                                    'datas': datas,
+                                                    'store_fname': zip_file,
+                                                    })
+
+                    # Create a task to delete the zip file (1 hour after this)
+                    cron_name = 'Delete useless zip file (%s)' % att.id
+                    model_id = self.env['ir.model'].sudo().search([
+                                                ('model','=',self._name)
+                                                ])
+                    self.env['ir.cron'].sudo().create({
+                                                    'name': cron_name,
+                                                    'model_id': model_id.id,
+                                                    'state': 'code',
+                                                    'code': 'model.pack_delete_useless_zip(%s)' % att.id,
+                                                    'interval_number': 3,
+                                                    'interval_type': 'minutes',
+                                                    'nextcall': datetime.datetime.now() \
+                                                                + datetime.timedelta(minutes = 30),
+                                                    'numbercall': 1,
+                                                    'doall': True
+                                                    })
+                except Exception as e:
+                    _logger.error("ZIP attachment: %s" % e)
+                    msg.append(_(e))
+                    pass
+
+            # Delete the source files
+            try:
+                shutil.rmtree(file_path)
+            except Exception as e:
+                _logger.info("Source files deletion: %s" % e)
+                _logger.info(file_path)
+                msg.append(_(e))
+                pass
+
+            print("zip_created@@@@@@@@@@@@@@@",zip_created)    
+
+            if zip_created:
+                return self.pack_redirect(att.id)
+            else:
+                _logger.error(_('A ZIP file was not created:\n%s') % '\n'.join(msg))
+                raise ValidationError(_('A ZIP file was not created:\n%s') % '\n'.join(msg))
+
+
     def _get_comment(self):
         rec_ids = self.pack_find_candidates()
-        if rec_ids:
+        if rec_ids:cron_name
             files = self.env['ir.attachment'].search([
                                                 ('res_model','=',self._context['active_model']),
                                                 ('res_id','in',[x.id for x in rec_ids])
