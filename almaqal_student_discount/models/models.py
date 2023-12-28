19e3a3757f9acdc82ecf1811cbabe84b348056b1
Anuj Chauhan <anuj.chauhan@finbii.com>
2023-12-28 14:28:05 +0530

    added

--- almaqal_student_discount/models/models.py
+++ almaqal_student_discount/models/models.py
@@ -433,8 +433,13 @@ class PaymentValue(models.Model):
                         name = rest.name
 
                         _logger.info("rest.reconciled_invoice_ids.invoice_payments_widget************11111111111111#####**%s" %inv.invoice_payments_widget)
+                        
+
                         for amont in json.loads(inv.invoice_payments_widget).get("content"):
                             totl_amount = amont.get("amount")
+
+                        if rest.reconciled_invoices_count == 1:
+                            totl_amount = rest.amount
 
                         worksheet.write(row, 3, rest.partner_id.name)
                         worksheet.write(row, 4, '{:,}'.format(int(totl_amount)))
