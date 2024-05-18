33ede3834fbeaf4f40e5f233c9f7443a6a28b7d9
Anuj Chauhan <anuj@techtime.me>
2024-05-18 14:01:20 +0530

    added

---
+++ almaaqal_fields/models/models.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api , _
+
+class ResPart(models.Model):
+    _inherit = "res.partner"
+
+    graduation_source = fields.Many2one("graduation.source", string="Graduation Source")
+    nationalty_source = fields.Many2one("nationality.source", string="Nationality")
+    second_nationality_source = fields.Many2one("nationality.source", string="Second Nationality")
+    remark_data_change_2 = fields.One2many("status.change","res_part_2",store = True)
+
+
+    def action_done_show_wizard_level_status(self):
+        return {'type': 'ir.actions.act_window',
+        'name': _('Change the Status'),
+        'res_model': 'status.change',
+        'target': 'new',
+        'view_id': self.env.ref('almaaqal_fields.view_any_name_form_status_change').id,
+        'view_mode': 'form',
+        'context': {"active_id" : self._context.get("active_ids")}
+        }
+
+
+class DataLevelStatus(models.TransientModel):
+    _name = 'status.change'
+
+    res_part_2 = fields.Many2one("res.partner")   
+    notes_data = fields.Text("Notes", track_visibility=True)
+    data_date_value = fields.Date("Date", track_visibility=True)
+    sequence_num = fields.Char("Sequence", track_visibility=True)
+    attachment = fields.Many2many("ir.attachment",  string="Attachment")
+
+    def action_confirm_change_level(self):
+        print("res@@@@@@@@@@@@@@@@@@@@@@@@@@26666",self._context.get("active_id"))
+        for idds in self._context.get("active_id"):
+            print("idds@@@@@@@@@@@@@@@@@",idds)
+            levels_sale_order = self.env["res.partner"].browse(int(idds))
+            print("levels_sale_order@@@@@@@@@@@@@@",levels_sale_order)
+            self.res_part_2 = levels_sale_order.id
+            levels_sale_order.remark_data_change_2  = (4, self.id)
+            print("levels_sale_order.remark_data_change_2@@@@@@@@@@",levels_sale_order.remark_data_change_2)
+     
+
+class TechtimeNewWork(models.Model):
+    _name = 'graduation.source'
+    _description = 'graduation.source'
+
+    name = fields.Char("نافذة القبول")      
+
+class TechtimeNationality(models.Model):
+    _name = 'nationality.source'
+    _description = 'nationality.source'
+
+    name = fields.Char("Name")      
+
+
+# class almaaqal_fields(models.Model):
+#     _name = 'almaaqal_fields.almaaqal_fields'
+#     _description = 'almaaqal_fields.almaaqal_fields'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
