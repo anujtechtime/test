d6741ad846930ef42a7e82ba04a7eb725018d40f
Anuj Chauhan <anuj@techtime.me>
2024-07-01 17:37:13 +0530

    added

--- almaaqal_fields/models/models.py
+++ almaaqal_fields/models/models.py
@@ -6,6 +6,11 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+class PayrollPay(models.Model):
+    _inherit = "hr.payroll"
+
+    iban = fields.Char("IBAN" , related="employee_id.iban")
+
 class ResPart(models.Model):
     _inherit = "res.partner"
 
