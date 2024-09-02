fb36229dc27109d5e2c90d8dbc54650f0130e27d
anujtechtime <anuj@techtime.me>
2024-09-02 18:00:23 +0530

    added

--- almaqal_student_discount/models/models.py
+++ almaqal_student_discount/models/models.py
@@ -171,18 +171,11 @@ class ResPrtner(models.Model):
     @api.model
     def create(self, vals):
         result = super(ResPrtner, self).create(vals)
-        duplicate_student = result.env["sale.order"].search([("partner_id","=",result.partner_id.id),("year","=",result.year.id)], limit=1)
-        if duplicate_student:
-            return {'warning': { 
-                    'title': "Warning", 
-                    'message': "يرجئ التاكد من عدم تكرار بطاقه التسجيل ", 
-                    } 
-                }
         installmet_dat = result.env["installment.details"].search([('college' , '=', result.college.id),("level","=",result.level),("Subject","=",result.Subject),('department','=',result.department.id),('Student','=',result.student.id),('year','=', result.year.id)],limit=1)
         instamm_ment_details = self.env["installment.details"].search([("student_dicount","=",True),('college','=',result.partner_id.college.id),("Student","=",result.student.id),("level","=",result.partner_id.level),('Subject','=',result.partner_id.shift),('year','=',result.partner_id.year.id),('department','=',result.partner_id.department.id),('percentage_from','<=',result.partner_id.final_result),('percentage_to','>=',result.partner_id.final_result)], limit=1)
         failed_student = self.env["sale.order"].search([("partner_id","=",result.partner_id.id),("college","=",result.partner_id.college.id),("year","!=",result.partner_id.year.id),("level","=",result.partner_id.level)], limit=1)
         _logger.info("failed_student************11111111111111#####**%s" %failed_student)
-        if failed_student:
+        elif failed_student:
             result.installment_amount = failed_student.installment_amount
             payemnt_date = installmet_dat.sale_installment_line_ids.mapped("payment_date")
 
@@ -205,7 +198,7 @@ class ResPrtner(models.Model):
                 })  
                 payemnt_date.remove(nearest_date)
         count = 0        
-        if not failed_student and installmet_dat:
+        elif not failed_student and installmet_dat:
             _logger.info("instamm_ment_detailsinstamm_ment_details11111111111111#####**%s" %instamm_ment_details)
             if instamm_ment_details:
                 result.installment_amount = instamm_ment_details.installment
@@ -237,7 +230,7 @@ class ResPrtner(models.Model):
                         result.amount = ('{:,}'.format(i.amount_installment))
                         count = count + 1        
 
-        if installmet_dat:
+        elif installmet_dat:
             print("sale_installment_line_ids########",installmet_dat.sale_installment_line_ids.ids)
             # for datts in installmet_dat.sale_installment_line_ids:
             # result.sale_installment_line_ids = [(6, 0, installmet_dat.sale_installment_line_ids.ids)]
@@ -260,23 +253,35 @@ class ResPrtner(models.Model):
         return result
 
 
-    @api.onchange('partner_id')
-    def _compute_partner_id(self):
-        if self.partner_id:
-            # if self.partner_id.shift >=  cgpa.shift and self.partner_id.student_cgpa >=  cgpa.percentage_from and self.partner_id.student_cgpa <=  cgpa.percentage_to:
-            instamm_ment_details = self.env["installment.details"].search([("student_dicount","=",True),('college','=',self.college.id),('Subject','=',self.partner_id.shift),('year','=',self.year.id),('department','=',self.department.id),('percentage_from','<=',self.partner_id.final_result),('percentage_to','>=',self.partner_id.final_result)])
-            self.installment_amount = instamm_ment_details.installment
-            if instamm_ment_details:
-                for i in instamm_ment_details.sale_installment_line_ids:
-                    sale_installment = self.sale_installment_line_ids.create({
-                        'number' : i.number,
-                        'payment_date' : i.payment_date,
-                        'amount_installment' : i.amount_installment,
-                        'description': 'Installment Payment',
-                        'sale_installment_id' : result.id,
-                        # "invoice_id" : invoice_id.id
-                        })
+    # @api.onchange('partner_id')
+    # def _compute_partner_id(self):
+    #     if self.partner_id:
+    #         # if self.partner_id.shift >=  cgpa.shift and self.partner_id.student_cgpa >=  cgpa.percentage_from and self.partner_id.student_cgpa <=  cgpa.percentage_to:
+    #         instamm_ment_details = self.env["installment.details"].search([("student_dicount","=",True),('college','=',self.college.id),('Subject','=',self.partner_id.shift),('year','=',self.year.id),('department','=',self.department.id),('percentage_from','<=',self.partner_id.final_result),('percentage_to','>=',self.partner_id.final_result)])
+    #         self.installment_amount = instamm_ment_details.installment
+    #         if instamm_ment_details:
+    #             for i in instamm_ment_details.sale_installment_line_ids:
+    #                 sale_installment = self.sale_installment_line_ids.create({
+    #                     'number' : i.number,
+    #                     'payment_date' : i.payment_date,
+    #                     'amount_installment' : i.amount_installment,
+    #                     'description': 'Installment Payment',
+    #                     'sale_installment_id' : result.id,
+    #                     # "invoice_id" : invoice_id.id
+    #                     })
 
+    @api.onchange('partner_id', 'year')
+    def ompute_partner_id(self):
+        print("self.partner_id@@@@@@@@@@@@@@@",self.partner_id)
+        if self.partner_id and self.year:
+            duplicate_student = self.env["sale.order"].search([("partner_id","=",self.partner_id.id),("year","=",self.year.id)], limit=1)
+            print("duplicate_student@@@@@@@@@@@@@@@@@@",duplicate_student)
+            if duplicate_student:
+                return {'warning': { 
+                        'title': "Warning", 
+                        'message': "يرجئ التاكد من عدم تكرار بطاقه التسجيل ", 
+                        } 
+                    }
 
 class DataLevelValue(models.TransientModel):
     _name = 'exipire.value'
