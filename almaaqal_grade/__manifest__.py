b64717e5b540ccf34a03197b1f47fe835895f134
anujtechtime <anuj@techtime.me>
2024-09-04 18:20:35 +0530

    added

--- almaaqal_grade/__manifest__.py
+++ almaaqal_grade/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'security/grade_security.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
