48d9b64fdd544608d62a0baa178847e5f37267ae
anujtechtime <anuj@techtime.me>
2024-10-09 13:04:36 +0530

    addd

--- almaqal_student_discount/models/models.py
+++ almaqal_student_discount/models/models.py
@@ -220,6 +220,7 @@ class ResPrtner(models.Model):
                         'description': 'Installment Payment',
                         'sale_installment_id' : result.id,
                         })
+                    payemnt_date.remove(nearest_date)    
             result.second_payment_date = datetime.today().date()
             order_line = result.env['sale.order.line'].create({
                 'product_id': 1,
