5308f066111b9ac507abe33da9c178fa8d946350
anujtechtime <anuj@techtime.me>
2025-02-06 10:38:30 +0530

    added

--- almaaqal_trial_balance/__manifest__.py
+++ almaaqal_trial_balance/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'techtime_mcc_data'],
+    'depends': ['base', 'techtime_mcc_data', 'sale', 'account'],
 
     # always loaded
     'data': [
--- almaaqal_trial_balance/models/models.py
+++ almaaqal_trial_balance/models/models.py
@@ -37,6 +37,12 @@ class MrpProductSale(models.Model):
 
     year_of_acceptance_1 = fields.Many2one("techtime_mcc_data.techtime_mcc_data", string="Year of acceptance", related="partner_id.year_of_acceptance_1")
 
+class MrpProductPayment(models.Model):
+    _inherit = 'account.payment'
+
+    casher = fields.Selection([('option1', 'حسين'),('option2','مسلم حسن'),('option3','مصطفى يوسف')], string="الكاشير")
+
+    casher_bool = fields.Boolean(string="اشتراك نادي الطلبة")
 
 class MrpProductWizard(models.TransientModel):
     _name = 'trail.balance.wizard'
