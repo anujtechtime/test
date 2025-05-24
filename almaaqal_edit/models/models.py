7c31e9f350afffd8a60dc737a86b5dc88731cce7
anujtechtime <anuj@techtime.me>
2025-05-24 13:13:22 +0530

    aaded

--- almaaqal_edit/models/models.py
+++ almaaqal_edit/models/models.py
@@ -16,7 +16,7 @@ class almaaqalStatusTypem(models.Model):
     status_type = fields.Many2one("status.type",string="Status Type")  
 
 class PersistentModelinher(models.Model):
-    _inherit = 'persistent.model'
+    _inherit = ['persistent.model', 'level.value']
 
     registration_status = fields.Many2one("registration.status")   
     status_type = fields.Many2one("status.type",string="Status Type")  
