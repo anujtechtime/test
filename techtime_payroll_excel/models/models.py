c2b9df5a642774366c0cf500351fff4168f6c1a1
anujtechtime <anuj@techtime.me>
2025-09-22 10:56:14 +0530

    added

--- techtime_payroll_excel/models/models.py
+++ techtime_payroll_excel/models/models.py
@@ -311,9 +311,11 @@ class techtime_payrollDepartment(models.Model):
 
             worksheet.write(row, 3, 'أسم الطالب', header_bold)
 
-            worksheet.write(row, 4, 'حالة الطالب', header_bold)
+            worksheet.write(row, 4, 'تاريخ السداد', header_bold)
 
-            worksheet.write(row, 5, 'المبلغ المتبقي', header_bold)
+            worksheet.write(row, 5, 'حالة الطالب', header_bold)
+
+            worksheet.write(row, 6, 'المبلغ المتبقي', header_bold)
 
             row = 3
             for level in level_data:
@@ -375,15 +377,28 @@ class techtime_payrollDepartment(models.Model):
                     invoice_data = self.filtered(lambda picking: picking.partner_id.Status == status and  picking.department.id == depp.id and picking.state == "posted" and picking.level == level and picking.invoice_payment_state != 'paid').sorted(key=lambda r: r.partner_id.name)
                 
                     sequence = 1
+                    same_name = None
+                    partner_total = 0.0 
 
                     if invoice_data:
                         worksheet.write(row - 1, 0, level_name   + "(" + str(len(invoice_data.mapped("id"))) + ")" , header_bold)
                         for inv in invoice_data:
+
+                            partner_name = inv.partner_id.name
+
+                            # If partner changes -> write total row for previous partner
+                            if same_name and partner_name != same_name:
+                                worksheet.write(row, 2, "Total", header_bold)
+                                worksheet.write(row, 6, partner_total, header_bold)
+                                row += 1
+                                partner_total = 0.0  # reset for new partner
+
                             print("inhhhhhhhhhhhhhhhhhhh",inv.partner_id.name)
                             print("row@@@@@@@@@@@@@@wwwwwwwwww",row)
                             worksheet.write(row, 1, sequence, main_cell)
 
                             worksheet.write(row, 2, inv.name, main_cell)
+
 
                             worksheet.write(row, 3, inv.partner_id.name, main_cell)
 
@@ -412,15 +427,27 @@ class techtime_payrollDepartment(models.Model):
                                 status_data  = "طالب منتقل من الجامعة"     
 
                             if status == "graduated":
-                                status_data  = "طالب ناجح"                            
+                                status_data  = "طالب ناجح"           
 
-                            worksheet.write(row, 4, status_data, main_cell)
+                            worksheet.write(row, 4, inv.invoice_date_due.strftime("%Y-%m-%d"), main_cell)                     
 
-                            worksheet.write(row, 5, inv.amount_residual, main_cell)
+                            worksheet.write(row, 5, status_data, main_cell)
+
+                            worksheet.write(row, 6, inv.amount_residual, main_cell)
 
                             row = row + 1
                             print("row@@@@@@@@@@@@@@eeeeeeeeee",row)
                             sequence = sequence + 1
+
+                            partner_total += inv.amount_residual
+
+                            # Update trackers
+                            same_name = partner_name
+
+                        if same_name:
+                            worksheet.write(row, 2, "Total", header_bold)
+                            worksheet.write(row, 6, partner_total, header_bold)
+                            row += 1
                         row = row + 3    
                         
 
@@ -449,7 +476,7 @@ class techtime_payrollDepartment(models.Model):
             "type": "ir.actions.act_url",
             "url": str(base_url) + str(download_url),
             "target": "new",
-        }        
+        }
 
 
     def send_mis_report_for_department_new_sheet(self):
