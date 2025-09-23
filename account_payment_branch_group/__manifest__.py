11b292bb23c8d4806128832f3927fec2bd5ae852
anujtechtime <anuj@techtime.me>
2025-09-23 17:37:57 +0530

    added

---
+++ account_payment_branch_group/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Account Payment Branch Group",
+    "version": "16.0.1.0.0",
+    "summary": "Add Branch Code field in users and group payments by Branch",
+    "category": "Accounting",
+    "author": "Your Name",
+    "depends": ["account"],
+    "data": [
+        "views/account_payment_views.xml"
+    ],
+    "installable": true,
+    "application": false,
+    "license": "LGPL-3"
+}
