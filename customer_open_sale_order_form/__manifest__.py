531a870d36c798e2b74324fe48eda5e907e54da9
anujtechtime <anuj@techtime.me>
2025-10-18 15:32:27 +0530

    addd

--- customer_open_sale_order_form/__manifest__.py
+++ customer_open_sale_order_form/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '13.0.1.0.0',
     'summary': 'Button on Customer form to open new Sale Order with pre-filled customer',
     'author': 'ChatGPT',
-    'depends': ['base', 'contacts', 'sale'],
+    'depends': ['base', 'contacts', 'sale', 'almaqal_templates'],
     'data': ['views/res_partner_view.xml'],
     'installable': True,
     'application': False,
