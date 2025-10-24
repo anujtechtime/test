7ee89b2e83f6087c35c38f54858aa63ccbf040cf
anujtechtime <anuj@techtime.me>
2025-10-24 15:36:47 +0530

    added

--- partner_stage_level/models/res_partner.py
+++ partner_stage_level/models/res_partner.py
@@ -17,3 +17,21 @@ class ResPartner(models.Model):
         string='المستوى',
         help='Customer level',
     )
+
+
+class RespersistentModel(models.Model):
+    _inherit = 'persistent.model'
+
+    stage_id = fields.Many2one(
+        'res.partner.stage',
+        string='المستوى',
+        help='Customer level',
+    )
+
+    def action_confirm_change(self):
+        res = super(RespersistentModel, self).action_confirm_change()
+        print("self########33333444444444444444",self.res_part)
+        if self.stage_id:
+            self.res_part.stage_id = self.stage_id.id
+        return res
+
