a28a063c7770f1b8cd28d6664f27bb313e72baff
anujtechtime <anuj@techtime.me>
2026-01-20 14:55:02 +0530

    addedd

--- docx_report_chatter_log/controllers/report.py
+++ docx_report_chatter_log/controllers/report.py
@@ -2,38 +2,39 @@ from odoo import http, _
 from odoo.http import request
 from odoo.addons.web.controllers.main import ReportController
 from datetime import datetime
-import json
-import base64
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class ReportControllerInherit(ReportController):
 
     @http.route(['/report/download'], type='http', auth='user')
     def report_download(self, data, context=None):
-        # Parse request
-        request_content = json.loads(data)
-        report_name = request_content[0]
-        report_type = request_content[1]
-        docids = request_content[2]
+        # 🔒 ALWAYS call super first (important in Odoo 13)
+        response = super().report_download(data, context=context)
 
-        ctx = request.env.context
-        active_model = ctx.get('active_model')
-        active_ids = ctx.get('active_ids')
+        try:
+            ctx = request.env.context or {}
+            active_model = ctx.get('active_model')
+            active_ids = ctx.get('active_ids')
 
-        # 🔥 LOG HERE (THIS ALWAYS FIRES)
-        if active_model and active_ids:
-            records = request.env[active_model].browse(active_ids)
-            user = request.env.user
-            now = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+            if active_model and active_ids:
+                records = request.env[active_model].browse(active_ids)
+                user = request.env.user
+                now = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
 
-            for rec in records:
-                if hasattr(rec, 'message_post'):
-                    rec.message_post(
-                        body=_(
-                            "<b>%s</b> downloaded %s report <i>%s</i> on %s"
-                        ) % (user.name, report_type.upper(), report_name, now),
-                        subtype_xmlid='mail.mt_note'
-                    )
+                for rec in records:
+                    if hasattr(rec, 'message_post'):
+                        rec.message_post(
+                            body=_(
+                                "<b>%s</b> downloaded a report on %s"
+                            ) % (user.name, now),
+                            subtype_xmlid='mail.mt_note'
+                        )
 
-        # Continue normal flow
-        return super().report_download(data, context=context)
+        except Exception as e:
+            # ❗ Never block report download
+            _logger.exception("Report download chatter log failed: %s", e)
+
+        return response
