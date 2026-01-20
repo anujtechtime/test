980abd74404747653726ccfccda531514d465f7b
anujtechtime <anuj@techtime.me>
2026-01-20 14:27:09 +0530

    addedd

--- partner_report_chatter_log/models/ir_actions_report.py
+++ partner_report_chatter_log/models/ir_actions_report.py
@@ -1,17 +1,27 @@
 from odoo import models, api, _
 from datetime import datetime
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class IrActionsReport(models.Model):
     _inherit = 'ir.actions.report'
 
-    def report_action(self, docids=None, data=None, config=True):
-        res = super().report_action(docids, data=data, config=config)
+    @api.model
+    def _get_report_from_name(self, report_name):
+        report = super()._get_report_from_name(report_name)
 
-        # ✅ Odoo 13 FIX: read IDs from context
-        active_ids = self.env.context.get('active_ids')
+        ctx = self.env.context
+        active_model = ctx.get('active_model')
+        active_ids = ctx.get('active_ids')
 
-        if self.model == 'res.partner' and active_ids:
+        _logger.warning(
+            "REPORT PRINT DETECTED | report=%s model=%s ids=%s",
+            report_name, active_model, active_ids
+        )
+
+        if active_model == 'res.partner' and active_ids:
             partners = self.env['res.partner'].browse(active_ids)
             user = self.env.user
             now = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
@@ -20,8 +30,8 @@ class IrActionsReport(models.Model):
                 partner.message_post(
                     body=_(
                         "<b>%s</b> printed report <i>%s</i> on %s"
-                    ) % (user.name, self.name, now),
+                    ) % (user.name, report.name, now),
                     subtype_xmlid='mail.mt_note'
                 )
 
-        return res
+        return report
