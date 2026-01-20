6546abed865c479889c7430d9b891bda78b7d8d4
anujtechtime <anuj@techtime.me>
2026-01-20 14:50:18 +0530

    addedd

--- docx_report_chatter_log/controllers/report.py
+++ docx_report_chatter_log/controllers/report.py
@@ -2,19 +2,25 @@ from odoo import http, _
 from odoo.http import request
 from odoo.addons.web.controllers.main import ReportController
 from datetime import datetime
+import json
+import base64
 
 
 class ReportControllerInherit(ReportController):
 
-    @http.route([
-        '/report/docx/<string:report_name>/<string:docids>',
-    ], type='http', auth='user')
-    def report_docx(self, report_name, docids=None, **data):
+    @http.route(['/report/download'], type='http', auth='user')
+    def report_download(self, data, context=None):
+        # Parse request
+        request_content = json.loads(data)
+        report_name = request_content[0]
+        report_type = request_content[1]
+        docids = request_content[2]
 
         ctx = request.env.context
         active_model = ctx.get('active_model')
         active_ids = ctx.get('active_ids')
 
+        # 🔥 LOG HERE (THIS ALWAYS FIRES)
         if active_model and active_ids:
             records = request.env[active_model].browse(active_ids)
             user = request.env.user
@@ -24,9 +30,10 @@ class ReportControllerInherit(ReportController):
                 if hasattr(rec, 'message_post'):
                     rec.message_post(
                         body=_(
-                            "<b>%s</b> printed DOCX report <i>%s</i> on %s"
-                        ) % (user.name, report_name, now),
+                            "<b>%s</b> downloaded %s report <i>%s</i> on %s"
+                        ) % (user.name, report_type.upper(), report_name, now),
                         subtype_xmlid='mail.mt_note'
                     )
 
-        return super().report_docx(report_name, docids, **data)
+        # Continue normal flow
+        return super().report_download(data, context=context)
