de7cfa1f6458f4d7f1451bc7023d0eb9ba028702
anujtechtime <anuj@techtime.me>
2026-05-21 11:25:31 +0530

    added

---
+++ contact_autocomplete_custom_search/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Contact Search',
+    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Custom contact search box with autocomplete',
+    'depends': ['web', 'contacts'],
+    'qweb': [
+        'static/src/xml/custom_contact_search.xml',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
