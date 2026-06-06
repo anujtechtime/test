7ad6bac5071530b8636271629d5d1567bccae971
anujtechtime <anuj@techtime.me>
2026-06-06 09:18:05 +0530

    added

--- partner_passport_number/models/res_partner.py
+++ partner_passport_number/models/res_partner.py
@@ -22,7 +22,7 @@ class TwchtTimet(models.Model):
     _inherit = 'account.move'
 
     Status = fields.Selection([('status4', 'مؤجل'),('status1','ترقين قيد'),('status2','طالب غير مباشر'),('status3','انسحاب'),('currecnt_student','Current student'),('succeeded','Succeeded'),('failed','Falied'),('transferred_from_us','Transferred From Us'),('graduated','Graduated')], string="Status", store=True, related="partner_id.Status")
-
+    year_of_acceptance_1 = fields.Many2one("techtime_mcc_data.techtime_mcc_data", string="Year of acceptance", store=True, related="partner_id.year_of_acceptance_1")
 
     # @api.model
     # def create(self, vals):
