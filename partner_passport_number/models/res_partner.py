4e9628c482abe565efac575f0bd77b5e5d27ba7a
anujtechtime <anuj@techtime.me>
2026-07-07 16:56:13 +0530

    added

--- partner_passport_number/models/res_partner.py
+++ partner_passport_number/models/res_partner.py
@@ -1,6 +1,11 @@
 from odoo import models, fields, api
 import re
 
+class TwchtTimet(models.Model):
+    _inherit = 'account.move'
+
+    student_status_in_department = fields.Char(string="حالة الطالب في القسم") 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
