361a9d08397bbeef8656c5704d44df676cb17d07
anujtechtime <anuj@techtime.me>
2026-07-07 14:59:33 +0530

    added

--- new_field_invoice/models/models.py
+++ new_field_invoice/models/models.py
@@ -22,3 +22,4 @@ class TwchtTimet(models.Model):
 
     Status = fields.Selection([('status4', 'مؤجل'),('status1','ترقين قيد'),('status2','طالب غير مباشر'),('status3','انسحاب'),('currecnt_student','Current student'),('succeeded','Succeeded'),('failed','Falied'),('transferred_from_us','Transferred From Us'),('graduated','Graduated')], string="Status", store=True, related="partner_id.Status")
     year_of_acceptance_1 = fields.Many2one("techtime_mcc_data.techtime_mcc_data", string="Year of acceptance", store=True, related="partner_id.year_of_acceptance_1")
+    student_status_in_department = fields.Char(string="حالة الطالب في القسم", store=True) 
