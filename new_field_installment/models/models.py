e53f2ca7e49bd5e5a5de0ac8dfbfbdcfd8683f3f
anujtechtime <anuj@techtime.me>
2026-07-08 14:35:26 +0530

    added

--- new_field_installment/models/models.py
+++ new_field_installment/models/models.py
@@ -9,25 +9,6 @@ class MoveDiv(models.Model):
     Status = fields.Selection([('status4', 'مؤجل'),('status1','ترقين قيد'),('status2','طالب غير مباشر'),('status3','انسحاب'),('currecnt_student','Current student'),('succeeded','Succeeded'),('failed','Falied'),('transferred_from_us','Transferred From Us'),('graduated','Graduated')], string="Status", store=True, related="partner_id.Status")
     year_of_acceptance_1 = fields.Many2one("techtime_mcc_data.techtime_mcc_data", string="Year of acceptance", store=True, related="partner_id.year_of_acceptance_1")
 
-class SaleInstallment(models.Model):
-    _inherit = 'sale.installment'
-
-    # department = fields.Many2one(
-    #     comodel_name='department.department',
-    #     string='Department',
-    #     compute='_compute_department',
-    #     store=True,        # Store for performance
-    #     readonly=True,
-    # )
-    department = fields.Many2one(
-        'department.department',
-        string='Department',
-    )
-
-    # @api.depends('invoice_id.department')
-    # def _compute_department(self):
-    #     for record in self:
-    #         record.department = record.invoice_id.department
 
 # class new_field_installment(models.Model):
 #     _name = 'new_field_installment.new_field_installment'
